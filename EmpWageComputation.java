0e20843f89427f68c0f8aa161286cb87c1a20a87
aravind789 <aravindkris89@gmail.com>
2020-07-17 13:28:38 +0530

    Added: code to check attandence

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,1 +1,16 @@
-Welcome
+public class EmpWageComputation {
+
+	public static void main(String[] args) {
+		int attendence=(int)(Math.random()*2);
+		//Checks if Employee is present or absent
+		if(attendence==1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+
+	}
+
+}
+
+
+
