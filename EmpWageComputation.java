2716d7faa909ee1c785141d58049747bbb5d46ce
aravind789 <aravindkris89@gmail.com>
2020-07-17 15:05:57 +0530

    Added:code to calculate parttime wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,13 +1,22 @@
-public class EmpWageComputation {
+ public class EmpWageComputation {
 	final static int WAGE_PER_HOUR=20;
 	static int dailyWage=0;
 	static int workHours=0;
+	public static int ComputeDailyWage(int hours) {
+		return hours*WAGE_PER_HOUR;
+	}
 	public static void main(String[] args) {
-		int attendence=(int)(Math.random()*2);
+		int attendence=(int)(Math.random()*3);
 		if(attendence==1)
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present full time");
 			workHours=8;
-			dailyWage=workHours*WAGE_PER_HOUR;
+			dailyWage=ComputeDailyWage(workHours);;
+	}
+		else if(attendence==1) {
+			System.out.println("Employee is Present for Part Time");
+			workHours=4;
+			dailyWage=ComputeDailyWage(workHours);;
+		}
 		else
 			System.out.println("Employee is Absent");
 		System.out.println("Daily Wage of an Employee "+dailyWage);
