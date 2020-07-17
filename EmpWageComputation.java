7dfc08f4fa6ee2e7bf04867d1e27c89fc6824b23
aravind789 <aravindkris89@gmail.com>
2020-07-17 13:37:50 +0530

    Added:Code for calculating wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,12 +1,16 @@
 public class EmpWageComputation {
-
+	final static int WAGE_PER_HOUR=20;
+	static int dailyWage=0;
+	static int workHours=0;
 	public static void main(String[] args) {
 		int attendence=(int)(Math.random()*2);
-		//Checks if Employee is present or absent
 		if(attendence==1)
 			System.out.println("Employee is Present");
+			workHours=8;
+			dailyWage=workHours*WAGE_PER_HOUR;
 		else
 			System.out.println("Employee is Absent");
+		System.out.println("Daily Wage of an Employee "+dailyWage);
 
 	}
 
